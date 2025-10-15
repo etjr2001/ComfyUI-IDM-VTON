f9fc7a5a320c8b19cd27749f7a84db4c7e2d09ba
etjr2001 <erictanjr21@gmail.com>
2025-10-15 13:51:30 +0800

    Modified original node to include original image as an output

--- src/nodes_mappings.py
+++ src/nodes_mappings.py
@@ -1,13 +1,16 @@
 from .nodes.pipeline_loader import PipelineLoader
 from .nodes.idm_vton import IDM_VTON
+from .nodes.idm_vton_v2 import IDM_VTON_V2
 
 
 NODE_CLASS_MAPPINGS = {
     "PipelineLoader": PipelineLoader,
     "IDM-VTON": IDM_VTON,
+    "IDM-VTON_V2": IDM_VTON_V2,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PipelineLoader": "Load IDM-VTON Pipeline",
     "IDM-VTON": "Run IDM-VTON Inference",
+    "IDM-VTON_V2": "IDM_VTON_V2"
 }
